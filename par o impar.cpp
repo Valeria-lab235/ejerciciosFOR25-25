0c5f17191493fdfcc9a19c95b5e39ef72098a2bc
Valeria-lab235 <valeriashuguli_31@hotmail.com>
2025-06-23 10:22:48 -0500

    Add files via upload

---
+++ par o impar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+bool esPar(int numero) {
+    return numero % 2 == 0;
+}
+int main() {
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+    if (esPar(n))
+        cout << "Es par." << endl;
+    else
+        cout << "Es impar." << endl;
+    return 0;
+}
